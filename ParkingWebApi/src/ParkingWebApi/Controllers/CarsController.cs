7c797e1bad08cf1efd530b34ce8588c4d75b9402
Yaroslav <zherebetskiy00@gmail.com>
2018-05-13 18:28:13 +0300

    Added car controller

---
+++ ParkingWebApi/src/ParkingWebApi/Controllers/CarsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ParkingLibrary;
+
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ParkingWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class CarsController : Controller
+    {
+        Parking parking = Parking.Instance;
+
+        [HttpGet]
+        public List<Car> GetAll()
+        {
+            var cars = parking.cars;
+            cars.Add(new Car("1", 123, CarType.Bus), new TransactionManager("1"));
+            return parking.cars.Keys.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public Car GetById(string id)
+        {
+            return parking.FindCarById(id);
+        }
+
+        [HttpPost]
+        public void Post([FromBody]Car car)
+        {
+            parking.AddCar(car);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(string id)
+        {
+            parking.RemoveCar(id);
+        }
+    }
+}
